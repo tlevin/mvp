fc8b7c6f939247fe1a466c8943a3dac83cecb53e
tlevin <toddlevin802@gmail.com>
2015-11-09 11:51:20 -0800

    Finish login view

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,2 +1,30 @@
-angular.module('myApp', [])
-  .config(function($stateProvider, $urlRouterProvider){});
+angular.module('app', ['app.login', 'ui.router'])
+  
+  .config(function($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('login', {
+        url: '/',
+        controller: 'LoginCtrl',
+        views: {
+          'main' : { templateUrl: 'partials/login.html' }
+        }
+      })
+      .state('generate', {
+        url: '/create',
+        controller: 'GenerateCtrl',
+        views: {
+          'nav' :  { templateUrl: 'partials/nav.html'},
+          'main' : { templateUrl: 'partials/generate.html' }
+        }
+      })
+      .state('list', {
+        url: '/list',
+        controller: 'ListController',
+        views: {
+          'nav' :  { templateUrl: 'partials/nav.html'},
+          'main' : { templateUrl: 'partials/generate.html' }
+        }
+      });
+    });
